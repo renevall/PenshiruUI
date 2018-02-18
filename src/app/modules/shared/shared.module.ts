43a1547f3483207217906abe4a4265e70de7abd0
Rene Vallecillo <reneval@gmail.com>
2018-02-17 19:10:03 -0600

    Working on left navbar

---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { NavbarLeftComponent } from './navbar-left/navbar-left.component';
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: [NavbarLeftComponent],
+  providers: []
+})
+export class SharedModule {}
